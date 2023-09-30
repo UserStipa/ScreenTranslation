6fa113c649592e71427d4e981a5a48cab97a971f
Stipa <945.kovdim@gmail.com>
2023-09-30 18:11:54 +0300

    feature: Select language fragment. Not completed. Datastore is done

---
+++ app/src/main/java/com/userstipa/screentranslation/data/DataStorePreferences.kt
@@ -0,0 +1,11 @@
+package com.userstipa.screentranslation.data
+
+import com.userstipa.screentranslation.models.Language
+import com.userstipa.screentranslation.models.LanguageType
+
+interface DataStorePreferences {
+
+    suspend fun putLanguage(languageType: LanguageType, language: Language)
+    suspend fun getLanguage(languageType: LanguageType): Language?
+
+}
---
+++ app/src/main/java/com/userstipa/screentranslation/data/DataStorePreferencesImpl.kt
@@ -0,0 +1,38 @@
+package com.userstipa.screentranslation.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.userstipa.screentranslation.data.DataStorePreferencesImpl.Companion.PREFERENCES_NAME
+import com.userstipa.screentranslation.models.Language
+import com.userstipa.screentranslation.models.LanguageType
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = PREFERENCES_NAME)
+
+class DataStorePreferencesImpl @Inject constructor(
+    private val context: Context
+) : DataStorePreferences {
+
+     override suspend fun putLanguage(languageType: LanguageType, language: Language) {
+        val preferencesKey = stringPreferencesKey(languageType.name)
+        context.dataStore.edit { preferences ->
+            preferences[preferencesKey] = language.code
+        }
+
+    }
+
+    override suspend fun getLanguage(languageType: LanguageType): Language? {
+        val preferencesKey = stringPreferencesKey(languageType.name)
+        val preferences = context.dataStore.data.first()
+        return Language.entries.firstOrNull { it.code == preferences[preferencesKey] }
+    }
+
+    companion object {
+        const val PREFERENCES_NAME = "PREFERENCES_NAME"
+    }
+}
